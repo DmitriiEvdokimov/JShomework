efe257de3cac92bb163c72e8cafebbd1f21aaa15
Dmitrii Evdokimov <whiteliquid913@gmail.com>
2023-04-28 22:58:20 +0300

    homework10.1 done

---
+++ homework101/data/data.js
@@ -0,0 +1,37 @@
+export const productData = `[{
+        "image": "img/copy.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00
+    },
+    {
+        "image": "img/copy-1.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00
+    },
+    {
+        "image": "img/copy-2.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00
+    },
+    {
+        "image": "img/copy-3.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00
+    },
+    {
+        "image": "img/copy-4.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00
+    },
+    {
+        "image": "img/copy-5.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00
+    }
+]`
