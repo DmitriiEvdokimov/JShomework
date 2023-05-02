97770db2f8baff2325ed87aae78213aa7792ab41
Dmitrii Evdokimov <whiteliquid913@gmail.com>
2023-05-02 18:41:08 +0300

    homework11 done

---
+++ homework11/data/data.js
@@ -0,0 +1,55 @@
+export const productData = `[{
+        "image": "img/copy.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "color" : "black",
+        "size" : 45,
+        "quantity" : 1
+    },
+    {
+        "image": "img/copy-1.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "color" : "red",
+        "size" :87,
+        "quantity" : 1
+    },
+    {
+        "image": "img/copy-2.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "color" : "blue",
+        "size" : 45,
+        "quantity" : 1
+    },
+    {
+        "image": "img/copy-3.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "color" : "black",
+        "size" : 45,
+        "quantity" : 1
+    },
+    {
+        "image": "img/copy-4.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "color" : "blue",
+        "size" : 78,
+        "quantity" : 1
+    },
+    {
+        "image": "img/copy-5.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "color" : "green",
+        "size" : 96,
+        "quantity" : 1
+    }
+]`
